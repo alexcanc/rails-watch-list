339bb03e7e6dc1107d59bfb9c25a0c4da6005b57
Alexandre Canacaris <tiloucanac1300@gmail.com>
2023-08-14 18:26:56 +0200

    in progress

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,6 +6,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    @movies = @list.movies  # Fetch movies associated with the list.
   end
 
   def new
