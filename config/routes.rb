66de238c84ad84995ede278983c8ad7fe46e5341
Alexandre Canacaris <tiloucanac1300@gmail.com>
2023-08-19 16:55:27 +0200

    Set root route to lists#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'lists#index'  # <-- Add this line
+
   resources :lists, only: [:index, :show, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create]
     resources :reviews, only: [:create]
