950c077b255cf5964abeb73369b0550be9dbca07
Alexandre Canacaris <tiloucanac1300@gmail.com>
2023-08-19 19:26:46 +0200

    Set root route to lists#index

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,37 +1,28 @@
 class ListsController < ApplicationController
-  # app/controllers/lists_controller.rb
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
+
   def index
-  if params[:list_query].present?
-    @lists = List.search_by_name(params[:list_query])
-  else
-    @lists = List.all
-  end
-  @bookmarks = Bookmark.where(list: @list)
+    if params[:list_query].present?
+      @lists = List.search_by_name(params[:list_query])
+    else
+      @lists = List.all
+    end
+    @bookmarks = Bookmark.where(list: @list)
   end
 
   def show
-  @list = List.find(params[:id])
-
-  if params[:movie_query].present?
-    @movies = @list.movies.search_by_title_and_overview(params[:movie_query])
-  else
-    @movies = @list.movies
-  end
-
-  @bookmarks = Bookmark.where(list: @list)
+    if params[:movie_query].present?
+      @movies = @list.movies.search_by_title_and_overview(params[:movie_query])
+    else
+      @movies = @list.movies
+    end
+    @bookmarks = Bookmark.where(list: @list)
   end
 
   def new
     @list = List.new
     # If you do want to create a bookmark for the list at the same time (not typical), you'll need:
     # @bookmark = Bookmark.new
-  end
-
-  def destroy
-    @list = List.find(params[:id])
-    @list.destroy
-
-    redirect_to lists_path, notice: 'List was successfully deleted.'
   end
 
   def create
@@ -44,24 +35,33 @@ class ListsController < ApplicationController
   end
 
   def edit
-    @list = List.find(params[:id])
+    # You've already set @list using before_action
     # If you do want to create a bookmark for the list at the same time (not typical), you'll need:
     # @bookmark = Bookmark.new
   end
 
   def update
-    @list = List.find(params[:id])
-    @list.update(list_params)
-    if @list.save
+    # You've already set @list using before_action
+    if @list.update(list_params)  # .update will save the model if validations pass
       redirect_to @list, notice: 'List was successfully updated.'
     else
       render :edit
     end
   end
 
+  def destroy
+    # You've already set @list using before_action
+    @list.destroy
+    redirect_to lists_path, notice: 'List was successfully deleted.'
+  end
+
   private
 
   def list_params
     params.require(:list).permit(:name, :image)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'lists#index'  # <-- Add this line
 
-  resources :lists, only: [:index, :show, :new, :create, :destroy] do
+  resources :lists, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :bookmarks, only: [:new, :create]
     resources :reviews, only: [:create]
   end
