e40df2082a034ef9eb46f926fb28454e4accb59f
Alexandre Canacaris <tiloucanac1300@gmail.com>
2023-08-19 17:01:55 +0200

    Set root route to lists#index

--- app/models/list.rb
+++ app/models/list.rb
@@ -11,6 +11,7 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
   has_many :reviews, dependent: :destroy
+  has_one_attached :image
 
 
   validates :name, presence: true, uniqueness: true
